15ca6a416d8cbd6311a7521938d38250ea42626d
ferocious <firrasaltaher@gmail.com>
2013-07-27 16:09:02 -0500

    Changes

--- Numbers/pi.py
+++ Numbers/pi.py
@@ -4,7 +4,7 @@ from math import *
 
 digits = raw_input('Enter number of digits to round PI to: ')
 
-# print ('{0:.%df}' % min(20, int(digits))).format(math.pi) # nested string formatting
+print ('{0:.%df}' % min(20, int(digits))).format(math.pi) # nested string format
 
 # calculate pi using Machin-like Formula
-print ('{0:.%df}' % min(30, int(digits))).format(4 * (4 * atan(1.0/5.0) - atan(1.0/239.0)))
+#print ('{0:.%df}' % min(30, int(digits))).format(4 * (4 * atan(1.0/5.0) - atan(1.0/239.0)))
